bb76179438944490715003e2605f7b05e1a57ae5
Sotiris Gkoulimaris <sotiris.gkoulimaris@flexpowerltd.com>
2020-10-21 13:34:32 +0100

    Duplicates bug

--- parser.py
+++ parser.py
@@ -12,13 +12,39 @@ def createDf():
     for fl in folder_list:
         files = glob.glob(fl+"/*.xlsx")
 
-        df = pd.DataFrame()
+        dfs = []
+        #df = pd.DataFrame()
         for f in files:
-            data = pd.read_excel(f)
-            print(data.loc[[0]])
-            #print(data)
+            name = f[:-5]
+            name = name.split("_", 1)[1]
+            #print(name)
+
+            data = pd.read_excel(f, header=5)
+            data = data[5:400]
+            data = data.reset_index(drop=True)
+
+            data =  data.dropna(1, how='all')
+            del data['Asset status']
+
+            #print(data.columns)
+            data.columns = data.columns.map(lambda x : name+"_"+x if x =='Actual (kWh)' or x == 'Baseline (kWh)' else x)
+            #data = data.add_prefix(name)
             #df.append(data)
-        
+            data = data.set_index('Date')
+            #print(data)
+            dfs.append(data)
+            print(data)
+            #df.append(data)
+        #print(dfs)
 
 
+        df = dfs[0]
 
+        for fs in dfs[1:]:
+            df = df.join(fs, how='outer')
+            #df = df.drop_duplicates()
+            #print(df)
+
+        output = fl.split("/")[2]
+        df.to_csv(output+".csv")
+        print("heyyyyy")
